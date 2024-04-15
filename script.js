a9f8f678e396afe50d3b3791f97e4e82d7a4e73e
mirkoterzic <mirko.terzic2002@gmail.com>
2024-04-15 19:38:13 +0200

    initialize game variables

--- script.js
+++ script.js
@@ -8,5 +8,22 @@ form.addEventListener("submit", (event) => {
   const formData = new FormData(form);
   const data = Object.fromEntries(formData);
   document.querySelector(".modal-wrapper").setAttribute("hidden", true);
-  console.log(data);
+  initializGame(data);
 });
+
+const initializeVariables = (data) => {
+  data.choice = +data.choice;
+  data.board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+  data.player1 = "X";
+  data.player2 = "0";
+  data.round = 0;
+  data.currentPlayer = "X";
+  data.gameOver = false;
+};
+
+const initializGame = (data) => {
+  //initialize game vartiables
+  initializeVariables(data);
+  console.log(data);
+  //add event listener to gameboard
+};
